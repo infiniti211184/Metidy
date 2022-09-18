316724e30529f1d7802925331165adfd06a81515
Julia Khramova <infiniti211184@mail.ru>
2022-09-18 15:03:06 +0300

    Дз Методы, может быть в строчке 19 была ошибка?

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,23 @@ public class Main {
         System.out.println(generatInstal(0, 2014));
         System.out.println("Для доставки потребуется дней" + calculateShipping(90));
     }
+
     public static boolean isLeap(int year) {
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
+
     public static String generatInstal(int os, int year) {
         if (!(os == 0 || os == 1)) {
-            throw new RuntimeException("устройство не поддерживает"); }
-            var currentYear = LocalDate.now().getYear();
-            String versionMessage = year < currentYear ? "установите облегченную версию" : "";
+            throw new RuntimeException("устройство не поддерживает");
+        }
+        var currentYear = LocalDate.now().getYear();
+        String versionMessage = year < currentYear ? "установите облегченную версию" : "";
+        {
             String osMessage = os == 0 ? "iOS" : "android";
             return String.format("Установите %s версию приложения по ссылке %s", versionMessage, osMessage);
         }
-        public static int calculateShipping (int distance){
+    }
+        public static int calculateShipping ( int distance){
             int start = 20;
             int finish = 40;
             int result = 1;
@@ -25,8 +30,4 @@ public class Main {
             return result;
         }
     }
-
-    
-
-
 
